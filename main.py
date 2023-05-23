5ef0f2df8d84c2a0e564964904d8500a573eaf92
sffdz <117524756+sffdz@users.noreply.github.com>
2023-05-23 12:24:12 +0200

    the same previeus code but with new message

--- main.py
+++ main.py
@@ -3,30 +3,27 @@ from functions import *
 def menu():
     print("\n==== Programming Quotes ====")
     print("1. Random quote")
-    print("2. All quotes")
-    print("3. display_quotes")
-    print("4. Exit")
-    
+    print("2. Display a number of quotes")
+    print("3. Exit")
+
 
 def main():
     while True:
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-4): ")
-        
+        choice = input("Choose your an action (1-3): ")
+
         if choice == "1":
             print_quote(random_quote(quotes))
         elif choice == "2":
-            view_quotes(quotes)
-        elif choice == "4":
-            print("Good bye...")
-        elif choice == "3":
              count = int(input("Enter the number of quotes to display: "))
              display_quotes(quotes, count)
+        elif choice == "3":
+            print("Good bye...")
         break
     else:
          print("Invalid input")
 
-if __name__ == "__main__":
+if name == "main":
     main()
